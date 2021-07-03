0707b2f5ebb78e693c86fdc5a7caad652c30d947
Alexander <shablovskiy91@mail.ru>
2021-07-04 00:52:14 +0300

    Example code writed

--- src/main/java/TomcatExample.java
+++ src/main/java/TomcatExample.java
@@ -1,2 +1,35 @@
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+
 public class TomcatExample {
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context context = tomcat.addContext("api", new File(".").getAbsolutePath());
+
+        tomcat.addServlet("/api", "main", new HttpServlet() {
+            @Override
+            protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+                Writer w = response.getWriter();
+                w.write("Product Star online! \n");
+                w.flush();
+                w.close();
+            }
+        });
+
+        context.addServletMappingDecoded("/*", "main");
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
 }
